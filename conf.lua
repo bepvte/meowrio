38b4d804d8f79e7178555d3c5c2a08e035b2b769
bep <8226605+bepvte@users.noreply.github.com>
2020-04-30 20:06:28 -0400

    setup releaser

--- conf.lua
+++ conf.lua
@@ -4,4 +4,19 @@ function love.conf(t)
     t.window.resizable = true          -- Let the window be user-resizable (boolean)
     t.window.fullscreen = true         -- Enable fullscreen (boolean)
     t.window.vsync = 1               -- Enable vertical sync (boolean)
+    t.releases = {
+        title = 'meowrio',
+        package = 'meowrio',
+        loveVersion = '11.3',
+        version = nil,
+        author = nil,
+        email = nil,
+        description = nil,
+        homepage = nil,
+        identifier = "co.bepbep.meowrio",
+        excludeFileList = { '/specs?/' , 'lib/bump/img/', 'lib/shack/game.png', '.*.rockspec', '.*.md', '*.yml', 'lib/hump/docs/.*'} ,
+        compile = true,
+        projectDirectory = '/home/bep/meowrio',
+        releaseDirectory = '/home/bep/meowrio/releases',
+    }
   end
